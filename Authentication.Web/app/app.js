e374b42637599b5a5634b4e604f29ebf3f23e711
JonasBrolin <jonas.brolin@outlook.com>
2014-08-15 10:26:19 +0200

    Initial auth completed

---
+++ Authentication.Web/app/app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('authApp', ['ngRoute', 'LocalStorageModule']);
+
+app.config(function($routeProvider) {
+    $routeProvider.when("/home",
+    {
+        controller: "homeController",
+        templateUrl: "app/views/home.html"
+    });
+
+    $routeProvider.when("/login",
+    {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+var serviceBase = 'http://localhost:60241/';
+//var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+app.constant('ngAuthSettings', {
+    apiServiceBaseUri: serviceBase,
+    clientId: 'JavaApp'
+});
---
+++ Authentication.Web/app/services/authService.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+app.factory('authService', ['$q','$http', 'localStorageService', 'ngAuthSettings', function($q, $http, localStorageService, ngAuthSettings) {
+    var serviceBase = ngAuthSettings.apiServiceBaseUri;
+    var authServiceFactory = {};
+
+    var _authentication = {
+        isAuth: false,
+        userName: ""
+    };
+
+    var _login = function(loginData) {
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password + "&client_id=" + ngAuthSettings.clientId;
+        var deffered = $q.defer();
+
+        $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function(response) {
+            localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName, refreshToken: response.refresh_token, useRefreshTokens: true });
+            _authentication.isAuth = true;
+            _authentication.userName = loginData.userName;
+
+            deffered.resolve(response);
+        }).error(function(err, status) {
+            deffered.reject(err);
+        });
+
+        return deffered.promise;
+    };
+
+    authServiceFactory.authentication = _authentication;
+    authServiceFactory.login = _login;
+    return authServiceFactory;
+
+} ])
